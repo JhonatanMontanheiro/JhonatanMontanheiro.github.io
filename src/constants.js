f9be8c11f185292c775f7900d786f8099aebeca1
Jhonatan Montanheiro <jhonatanmontanheiro@gmail.com>
2024-04-22 08:59:53 -0300

    versao final

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `Esse e meu PC, onde trabalho e estudo. No meu tempo livre gosto de jogar tambem.
+  Atualmente eu estudo principalmente javascript, HTML, CSS e o basico sobre banco de dados ja que faz parte do meu curriculo na escola.
+  Contudo ainda não sei se e isso que desejo perseguir, apesar de eu certamente gostar de front-end. Tenho certeza que encontrar meu primeiro trabalho como desenvolvedor vai me ajudar a entender as possibilidades.`,
+  
+  "sofa-table": `As vezes pode ser cansativo passar o tempo todo sentado então faço pausas no sofa pra esticar os ossos.`,
+  tv: `Na verdade eu nao tenho TV, mas costumo assistir bastante youtube pra estudar ou me entreter, alguns dos meus canais favoritos sao:
+   <a href="https://www.youtube.com/@CursoemVideo" target="_blank">CursoemVideo</a>, <a href="https://www.youtube.com/@YesTheory" target="_blank">YesTheory</a>,
+  <a href="https://www.youtube.com/@LatteASMR" target="_blank">LatteASMR</a> (pra nao me estressar) e <a href="https://www.youtube.com/@CodeAesthetic" target="_blank">CodeAesthetic</a>!`,
+  bed: `Alem de ser onde durmo, muitas vezes e onde tenho boas ideias sobre problemas que nao resolvi ou projetos que desejo criar.`,
+  resume: `Minha mesa e curriculo que você pode ler: <a href="https://www.linkedin.com/in/jhonatan-montanheiro-970b101b9/" target="_blank">Aqui</a>
+  Para contato rapido você pode utilizar meu whatsapp: (11) 9.4899-1008 estou disponível pra discutir oportunidades de trabalho.`,
+  projects: `Esse jogo/portfolio foi feito usando a biblioteca Kaboom.js que permite mapear e animar imagens sem nenhum motor externo.
+  O texto e alguns detalhes da organização sao tratados com HTML/CSS. Pude realizar esse projeto me apoiando em conhecimentos previos sobre web dev e com recursos e tutoriais disponibilizados pelo @JSLegend, obrigado!.`,
+  library: `Gosto de ler mas a realidade e que praticamente nao tenho livros físicos. Porem sempre gostei do ambiente tranquilo de bibliotecas e a ideia de ter uma estante dessas em casa :D`,
+  exit: `Para sair apenas feche a janela, obrigado pela visita!`, 
+  "cs-degree": `Estou trabalhando no meu TCC a ser apresentado em dezembro de 2024 pra obter meu diploma tecnico em analise e desenvolvimento de sistemas.`,
+  
+};
